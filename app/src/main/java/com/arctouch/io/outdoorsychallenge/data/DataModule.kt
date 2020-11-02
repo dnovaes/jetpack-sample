adc607b0fa69421eff89ff025cc79160e4c72aea
dnovaes <novaesdigo@gmail.com>
2020-11-02 01:39:30 -0300

    Move set of database to dataModule and fix reference of vehicle dao

--- app/src/main/java/com/arctouch/io/outdoorsychallenge/data/DataModule.kt
+++ app/src/main/java/com/arctouch/io/outdoorsychallenge/data/DataModule.kt
@@ -1,11 +1,13 @@
 package com.arctouch.io.outdoorsychallenge.data
 
+import androidx.room.Room
 import com.arctouch.io.outdoorsychallenge.data.source.database.IVehicleDatabaseSource
 import com.arctouch.io.outdoorsychallenge.data.source.database.VehicleDatabaseSource
 import com.arctouch.io.outdoorsychallenge.data.source.remote.outdoorsy.IVehicleRemoteDataSource
 import com.arctouch.io.outdoorsychallenge.data.source.remote.outdoorsy.VehicleRemoteDataSource
 import com.arctouch.io.outdoorsychallenge.data.source.remote.outdoorsy.network.OutdoorsyClientBuilder
 import com.arctouch.io.outdoorsychallenge.domain.model.factory.VehicleFactory
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val networkModule = module {
@@ -19,14 +21,20 @@ val dataSourceModule = module {
             factory = get<VehicleFactory>()
         )
     }
-}
 
-val databaseModule = module {
     single<IVehicleDatabaseSource> {
         VehicleDatabaseSource(
-            vehicleDao = get()
+            vehicleDao = get<VehicleDatabase>().vehicleDao()
         )
+    }
+
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            VehicleDatabase::class.java,
+            VehicleDatabase.TABLE_NAME
+        ).build()
     }
 }
 
-val dataModule = networkModule + dataSourceModule + databaseModule
+val dataModule = networkModule + dataSourceModule
