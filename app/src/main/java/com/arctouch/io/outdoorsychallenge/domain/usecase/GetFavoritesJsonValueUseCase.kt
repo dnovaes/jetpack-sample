49fc69711c103b9ed34ab3a5e09f0078e1d4eff5
mariomartins-ifood <h2mario@gmail.com>
2021-01-13 16:55:24 -0300

    Fix style

--- app/src/main/java/com/arctouch/io/outdoorsychallenge/domain/usecase/GetFavoritesJsonValueUseCase.kt
+++ app/src/main/java/com/arctouch/io/outdoorsychallenge/domain/usecase/GetFavoritesJsonValueUseCase.kt
@@ -2,13 +2,11 @@ package com.arctouch.io.outdoorsychallenge.domain.usecase
 
 import com.arctouch.io.outdoorsychallenge.domain.model.Vehicle
 import com.arctouch.io.outdoorsychallenge.domain.repository.IVehicleRepository
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import java.lang.reflect.Type
 
 interface GetFavoritesJsonValueUseCase {
-
     suspend operator fun invoke(): String
 }
 
@@ -16,13 +14,12 @@ class GetFavoritesJsonValue constructor(
     private val repository: IVehicleRepository,
     private val moshi: Moshi
 ) : GetFavoritesJsonValueUseCase {
-
     override suspend fun invoke(): String {
         val type: Type = Types.newParameterizedType(
             MutableList::class.java,
             Vehicle::class.java
         )
-        val adapter: JsonAdapter<List<*>> = moshi.adapter(type)
+        val adapter = moshi.adapter<List<Vehicle>>(type)
 
         return adapter.toJson(repository.getFavoriteVehicles())
     }
--- app/src/main/java/com/arctouch/io/outdoorsychallenge/domain/usecase/GetVehicleListFromJsonUseCase.kt
+++ app/src/main/java/com/arctouch/io/outdoorsychallenge/domain/usecase/GetVehicleListFromJsonUseCase.kt
@@ -1,14 +1,12 @@
 package com.arctouch.io.outdoorsychallenge.domain.usecase
 
 import com.arctouch.io.outdoorsychallenge.domain.model.Vehicle
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import timber.log.Timber
 import java.io.IOException
 import java.lang.reflect.Type
-
 
 interface GetVehicleListFromJsonUseCase {
     operator fun invoke(json: String): List<Vehicle>
@@ -20,10 +18,10 @@ class GetVehicleListFromJson constructor(private val moshi: Moshi) : GetVehicleL
             MutableList::class.java,
             Vehicle::class.java
         )
-        val adapter: JsonAdapter<List<*>> = moshi.adapter(type)
+        val adapter = moshi.adapter<List<Vehicle>>(type)
 
         try {
-            return adapter.fromJson(json)?.map { it as Vehicle } ?: emptyList()
+            return adapter.fromJson(json) ?: emptyList()
         } catch (e: IOException) {
             Timber.tag("QrCodeUtils").d("QRCode parse error: ${e.message}")
         } catch (e: JsonDataException) {
